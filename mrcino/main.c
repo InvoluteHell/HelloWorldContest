64e948526d7dd8382acac1e9b50c4da54ea1ac03
mrcino <96339857+mrcino@users.noreply.github.com>
2021-12-18 21:39:55 +0800

    Update main.c
    * 嘿（复读一遍：如果中奖了麻烦发消息到邮箱，谢谢~）

--- mrcino/main.c
+++ mrcino/main.c
@@ -1,6 +1,5 @@
 //x86
-
-int main()
+main()
 {
     int buf[256];
     *(int*)((int)buf + 0) = 72;
@@ -9,13 +8,11 @@ int main()
     *(int*)((int)buf + 3) = 108;
     *(int*)((int)buf + 4) = 111;
     *(int*)((int)buf + 5) = 32;
-
     *(int*)((int)buf + 6) = 119;
     *(int*)((int)buf + 7) = 111;
     *(int*)((int)buf + 8) = 114;
     *(int*)((int)buf + 9) = 108;
     *(int*)((int)buf + 10) = 100;
     *(int*)((int)buf + 11) = 0;
-
     puts(buf);
 }
