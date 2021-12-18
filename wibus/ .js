b960fc2a6375330dfa0254266b94cd0076fbce7d
wibus-wee <1596355173@qq.com>
2021-12-18 19:22:19 +0800

    @wibus-wee's answer

---
+++ wibus/ .js
@@ -0,0 +1,1 @@
+let result="",a=["01001000","01100101","01101100","01101100","01101111","00100000","01010111","01101111","01110010","01101100","01100100"];for(var i=0;i<a.length;i++){result+=String.fromCharCode(parseInt(a[i],2))}console.log(result);
